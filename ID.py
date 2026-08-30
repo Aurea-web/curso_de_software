aa1a5fd7d439824c5b0119383111b119c1a96d95
Aurea-web <willianeernandez@gmail.com>
2026-08-29 19:56:13 -0600

    Cree el archivo ID.py e incorpere un codigo que permite concatenar cadenas de texto para imprimir un ID.

---
+++ ID.py
@@ -0,0 +1,11 @@
+# Datos de la persona
+nombre = "Willian"
+apellido = "Hernández"
+edad = 22
+dui = "12345678-9"
+
+# Concatenar información para formar un ID
+id_persona = nombre + "_" + apellido + "_" + str(edad) + "_" + dui
+
+# Mostrar el ID
+print("ID generado:", id_persona)
